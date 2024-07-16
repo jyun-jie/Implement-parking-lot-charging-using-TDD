808b010e8ea64e0ad1b30f3f1da460eba685e896
jyun-jie <chenjiegaexpa@gmail.com>
2024-07-16 23:47:33 +0800

    extract temporary method

--- src/test/java/com/example/parkinglotcharge/ParkingFeeCalculatorTest.java
+++ src/test/java/com/example/parkinglotcharge/ParkingFeeCalculatorTest.java
@@ -7,68 +7,43 @@ import java.time.LocalDateTime;
 
 
 public class ParkingFeeCalculatorTest {
+    @Test
+    void over_150_minute_then_pay_150(){
+        runTest("2024-01-01T00:00:00","2024-01-01T02:30:00",150L);
+    }
+
+    private static void runTest(String startText,String endText ,long expected) {
+        ParkingFeeCalculator cal = new ParkingFeeCalculator();
+
+        long actual = cal.calcualte(
+                LocalDateTime.parse(startText),
+                LocalDateTime.parse(endText)
+        );
+
+
+        Assertions.assertThat( actual ).isEqualTo(expected);
+    }
 
     @Test
     void _15_minute_Free(){
-        ParkingFeeCalculator cal = new ParkingFeeCalculator();
-
-        LocalDateTime start = LocalDateTime.of(2024,1,1,0,0,0);
-        LocalDateTime end = LocalDateTime.of(2024,1,1,0,0,0);
-
-        long actual = cal.calcualte(start,end);
-
-        Assertions.assertThat( actual ).isEqualTo(0L);
+        runTest("2024-01-01T00:00:00","2024-01-01T00:14:59",0L);
     }
 
     @Test
     void over_15_minute_Not_Free(){
-        ParkingFeeCalculator cal = new ParkingFeeCalculator();
-
-        long actual = cal.calcualte(
-                LocalDateTime.parse("2024-01-01T00:00:00"),
-                LocalDateTime.parse("2024-01-01T00:15:00")
-        );
-
-
-        Assertions.assertThat( actual ).isEqualTo(30L);
+        runTest("2024-01-01T00:00:00","2024-01-01T00:15:00",30L);
     }
 
     @Test
     void over_30_minute_then_pay_60(){
-        ParkingFeeCalculator cal = new ParkingFeeCalculator();
-
-        long actual = cal.calcualte(
-                LocalDateTime.parse("2024-01-01T00:00:00"),
-                LocalDateTime.parse("2024-01-01T00:30:00")
-        );
-
-
-        Assertions.assertThat( actual ).isEqualTo(60L);
+        runTest("2024-01-01T00:00:00","2024-01-01T00:30:00",60L);
     }
 
     @Test
     void over_60_minute_then_pay_90(){
-        ParkingFeeCalculator cal = new ParkingFeeCalculator();
-
-        long actual = cal.calcualte(
-                LocalDateTime.parse("2024-01-01T00:00:00"),
-                LocalDateTime.parse("2024-01-01T01:00:00")
-        );
-
-
-        Assertions.assertThat( actual ).isEqualTo(90L);
+        runTest("2024-01-01T00:00:00","2024-01-01T01:00:00",90L);
     }
 
-    @Test
-    void over_150_minute_then_pay_150(){
-        ParkingFeeCalculator cal = new ParkingFeeCalculator();
-
-        long actual = cal.calcualte(
-                LocalDateTime.parse("2024-01-01T00:00:00"),
-                LocalDateTime.parse("2024-01-01T02:30:00")
-        );
 
 
-        Assertions.assertThat( actual ).isEqualTo(150L);
-    }
 }
