53ee5bd7fdefa2caadc425f7b280399cbc2aa1b1
jyun-jie <chenjiegaexpa@gmail.com>
2024-07-17 00:49:28 +0800

    extract method

--- src/main/java/com/example/parkinglotcharge/ParkingFeeCalculator.java
+++ src/main/java/com/example/parkinglotcharge/ParkingFeeCalculator.java
@@ -4,9 +4,12 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.time.Duration;
 import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
 
 public class ParkingFeeCalculator {
+
+    private Duration THIRTY_MINUTES = Duration.ofMinutes(30L);
+    private Duration FIFTEEN_MINUTES = Duration.ofMinutes(15L);
+
     //topic : 如何製作一個 能上新聞的 汽車計算機
     //停車場
     //15分鐘免費
@@ -20,21 +23,26 @@ public class ParkingFeeCalculator {
 
         Duration duration = Duration.between(start,end);
 
-        if (duration.compareTo(Duration.ofMinutes(15L))<=0) {
+        if (isShort(duration)) {
             return 0L;
         }
 
-        Duration thirtyMinutes = Duration.ofMinutes(30L);
-
-        long periods = BigDecimal.valueOf(duration.toNanos())
-                .divide(BigDecimal.valueOf(thirtyMinutes.toNanos()), RoundingMode.UP)
-                .longValue();
-        long fee = periods * 30;
+        long fee = getRegularFee(duration);
 
         return  Math.min(fee,150L);
     }
 
+    private long getRegularFee(Duration duration) {
+        long periods = BigDecimal.valueOf(duration.toNanos())
+                .divide(BigDecimal.valueOf(THIRTY_MINUTES.toNanos()), RoundingMode.UP)
+                .longValue();
+        long fee = periods * 30;
+        return fee;
+    }
 
+    private boolean isShort(Duration duration) {
+        return duration.compareTo(FIFTEEN_MINUTES) <= 0;
+    }
 
 
 }
