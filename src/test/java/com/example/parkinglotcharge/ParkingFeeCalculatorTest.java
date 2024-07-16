9d6eb332f895c7e60e7fa312192eda4c7c3dffdb
jyun-jie <chenjiegaexpa@gmail.com>
2024-07-17 00:04:54 +0800

    extract methods

--- src/test/java/com/example/parkinglotcharge/ParkingFeeCalculatorTest.java
+++ src/test/java/com/example/parkinglotcharge/ParkingFeeCalculatorTest.java
@@ -1,27 +1,57 @@
 package com.example.parkinglotcharge;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
 
 
 public class ParkingFeeCalculatorTest {
+
+    private static LocalDateTime start;
+    private static LocalDateTime end;
+    private static long actual;
+    private static ParkingFeeCalculator cal;
+
+
+    @BeforeEach
+    void setUp() {
+        cal = new ParkingFeeCalculator();
+    }
+
     @Test
     void over_150_minute_then_pay_150(){
         runTest("2024-01-01T00:00:00","2024-01-01T02:30:00",150L);
     }
 
-    private static void runTest(String startText,String endText ,long expected) {
-        ParkingFeeCalculator cal = new ParkingFeeCalculator();
+    private static void runTest(String startText, String endText , long expected) {
+        given_parking_starts_at(startText);
+        given_parking_ends_at(endText);
 
-        long actual = cal.calcualte(
-                LocalDateTime.parse(startText),
-                LocalDateTime.parse(endText)
-        );
+        when_calculate();
 
 
+        then_should_pay(expected);
+    }
+
+    private static void then_should_pay(long expected) {
         Assertions.assertThat( actual ).isEqualTo(expected);
+    }
+
+    private static void given_parking_ends_at(String endText) {
+        end = LocalDateTime.parse(endText);
+    }
+
+    private static void given_parking_starts_at(String startText) {
+        start = LocalDateTime.parse(startText);
+    }
+
+    private static void when_calculate() {
+        actual = cal.calcualte(
+                start,
+                end
+        );
     }
 
     @Test
