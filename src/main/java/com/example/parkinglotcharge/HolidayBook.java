faf5b550e754f53957513e1ab6270dc04f8f8d90
jyun-jie <chenjiegaexpa@gmail.com>
2024-07-18 01:01:01 +0800

    national holiday is a field to a holidayBook

--- src/main/java/com/example/parkinglotcharge/HolidayBook.java
+++ src/main/java/com/example/parkinglotcharge/HolidayBook.java
@@ -7,10 +7,15 @@ import java.util.List;
 import java.util.Set;
 
 public class HolidayBook {
+
+    private static Set<LocalDate> nationalHoliday = new HashSet<>();
+
+    public HolidayBook() {
+        nationalHoliday.add(LocalDate.of(2024,1,1));
+    }
+
     static boolean isHoliday(LocalDate today) {
 
-        Set<LocalDate> nationalHoliday = new HashSet<>();
-        nationalHoliday.add(LocalDate.of(2024,1,1));
         return nationalHoliday.contains(today) ||
                 List.of(DayOfWeek.SATURDAY, DayOfWeek.SUNDAY).contains(today.getDayOfWeek());
     }
